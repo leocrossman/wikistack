82fe2f0c4d77a0d880d3825564099ce6435d08af
leocrossman <lkcrossman@gmail.com>
2018-06-15 11:53:37 -0400

    current

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const main = require('./views/main.js');
 
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/stylesheets'));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
   try {
-    res.send('Hello World!');
+    res.send(main(''));
   } catch (error) {
     res.status(500).send(`Something went wrong: ${error}`);
   }
 });
 
-const PORT = 1337;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
