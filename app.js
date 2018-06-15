9718a7690926d068d0dd7725f76b5e8f167bce3b
leocrossman <lkcrossman@gmail.com>
2018-06-15 11:39:40 -0400

    added views folder

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', async (req, res) => {
+  try {
+    res.send('Hello World!');
+  } catch (error) {
+    res.status(500).send(`Something went wrong: ${error}`);
+  }
+});
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
